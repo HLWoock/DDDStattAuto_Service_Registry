d33633ecb42e1fdf5903ab080383e52aa2e1a530
HermannW <HermannW@Noose-1099.oosede.local>
2017-06-12 22:27:44 +0200

    initial

---
+++ src/main/java/de/woock/ddd/stattauto/registry/DddStattAutoSrvServiceRegistryApplication.java
@@ -0,0 +1,14 @@
+package de.woock.ddd.stattauto.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DddStattAutoSrvServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddStattAutoSrvServiceRegistryApplication.class, args);
+	}
+}
