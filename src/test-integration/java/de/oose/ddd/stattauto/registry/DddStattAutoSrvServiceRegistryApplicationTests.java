d01cc3012aae19ab1b49ff76ec95bfd45e418e6f
sh <sh@oose.de>
2017-08-12 00:32:45 +0200

    use random port for integration tests

--- src/test-integration/java/de/oose/ddd/stattauto/registry/DddStattAutoSrvServiceRegistryApplicationTests.java
+++ src/test-integration/java/de/oose/ddd/stattauto/registry/DddStattAutoSrvServiceRegistryApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class DddStattAutoSrvServiceRegistryApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
